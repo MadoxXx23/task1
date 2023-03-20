db6ae961a419cce5108ad0aa773d6ec88b0fcf0c
Belous <madoxxx24@gmail.com>
2023-03-20 22:31:17 +0300

    рефакторинг кода

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-from business.weather import main
+import requests
 
 
-if __name__=='__main__':
+URL='https://wttr.in'
+PLACES = ['Лондон', 'Череповец', 'SVO']
+def get_weather(places: list):
+    params = {
+        'lang': 'ru',
+        'n': '',
+        'T': '',
+        'q': ''
+
+    }
+    temp_list = []
+    for place in places:
+        req = requests.get(f"{URL}/{place}", params=params)
+        if req.status_code == 200:
+            temp_list.append(req.text)
+        else:
+            print(f'Не удалось получить данные с сервера, т.к. код ответа сервера {req.status_code}')
+    return temp_list
+
+
+def main():
+    weathers = get_weather(PLACES)
+    for weather in weathers:
+        print(weather)
+
+if __name__=="__main__":
     main()
